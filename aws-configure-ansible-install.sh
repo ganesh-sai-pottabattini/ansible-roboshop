1712a6f41db15efa06bc6095e838307c8beabd91
Sai <Sai@gmail.com>
2026-06-09 21:58:59 +0530

    mysql-rabbitmq-catalogue

--- aws-configure-ansible-install.sh
+++ aws-configure-ansible-install.sh
@@ -7,19 +7,34 @@ echo
 aws configure set aws_access_key_id "$ACCESS_KEY"
 aws configure set aws_secret_access_key "$SECRET_KEY"
 aws configure set region us-east-1
-
 aws configure set output json
+
+echo " ==========aws config and creds ================================================================================================= "
 
 cat ~/.aws/config
 cat ~/.aws/credentials
 
+echo " ======== Install boto3 botocore netaddr ======================================================================================== "
+
 pip3.9 install boto3 botocore netaddr
+
+echo " ============ Install Ansible =================================================================================================== "
 
 sudo dnf install ansible -y
 
-ansible-galaxy collection install amazon.aws
+# echo " ================================================================================================================== "
+
+# ansible-galaxy collection install amazon.aws
+
+echo " ========= Ansible Version ==================================================================================== "
 
 ansible --version
+
+echo " ========= mk folder , chng ownershop and permissions ==================================================================================== "
+
+sudo mkdir /var/log/roboshop/
+sudo chown -R ec2-user:ec2-user /var/log/roboshop/ansible.log
+sudo chmod 755 -R /var/log/roboshop/ansible.log
 
 #read -p "Access Key: " ACCESS_KEY
 # read -s -p "Secret Key: " SECRET_KEY
